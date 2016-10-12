1c210b7eac7b308c45c0d47e4bc36cd983360b18
Kenneth Lo <closer.tw@gmail.com>
2016-10-12 17:23:28 +0800

    Initial Commit

---
+++ CosmicSDL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CosmicSDL
+//
+//  Created by Kenneth Lo on 2016/10/12.
+//  Copyright © 2016年 L's Studio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
