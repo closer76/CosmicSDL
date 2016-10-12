de2824a9b36e53588a31f215ea542f8135b05b83
Kenneth Lo <closer.tw@gmail.com>
2016-10-12 18:08:36 +0800

    SDL Tutorial Lesson 2.
    Load and blit some images.

--- CosmicSDL/main.cpp
+++ CosmicSDL/main.cpp
@@ -17,51 +17,92 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main( int argc, char* args[] )
+// Global variables
+//The window we'll be rendering to
+SDL_Window* g_window = nullptr;
+
+//The surface contained by the window
+SDL_Surface* g_screenSurface = nullptr;
+
+SDL_Surface* g_hellowWorldSurface = nullptr;
+
+bool init()
 {
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
-    
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
-    
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        return false;
     }
     else
     {
         //Create window
-        window = SDL_CreateWindow( "SDL Tutorial",
+        g_window = SDL_CreateWindow( "SDL Tutorial",
                                   SDL_WINDOWPOS_UNDEFINED,
                                   SDL_WINDOWPOS_UNDEFINED,
                                   SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
+        if( g_window == NULL )
         {
             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            return false;
         }
         else
         {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-            
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0x00, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
-            
-            //Wait two seconds
-            SDL_Delay( 2000 );
+            // Get window surface
+            g_screenSurface = SDL_GetWindowSurface(g_window);
         }
     }
     
-    //Destroy window
-    SDL_DestroyWindow( window );
+    return true;
+}
+
+bool loadMedia()
+{
+    g_hellowWorldSurface = SDL_LoadBMP("hello_world.bmp");
+    if (!g_hellowWorldSurface) {
+        printf("Unable to load image %s! SDL Error: %s\n",
+               "02_getting_an_image_on_the_screen/hello_world.bmp",
+               SDL_GetError());
+        return false;
+    }
     
-    //Quit SDL subsystems
+    return true;
+}
+
+void terminate()
+{
+    // Release surface
+    SDL_FreeSurface(g_hellowWorldSurface);
+    g_hellowWorldSurface = nullptr;
+    
+    // Destroy window
+    SDL_DestroyWindow( g_window );
+    g_window = nullptr;
+    
+    // Quit SDL subsystems
     SDL_Quit();
+}
+
+int main( int argc, char* args[] )
+{
+    if (!init())
+    {
+        printf("Failed to initialize...\n");
+    }
+    else
+    {
+        // Load media
+        if (!loadMedia()) {
+            printf("Failed to load media...\n");
+        } else {
+            SDL_BlitSurface(g_hellowWorldSurface, NULL, g_screenSurface, NULL);
+            
+            SDL_UpdateWindowSurface(g_window);
+            
+            SDL_Delay(2000);
+        }
+    }
+    
+    terminate();
     
     return 0;
 }
